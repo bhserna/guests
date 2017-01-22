e3d80e401f6b6e9310944b524ffd10fb29385974
bhserna <bhserna@gmail.com>
2017-01-22 17:14:30 -0600

    capture leads

--- app.rb
+++ app.rb
@@ -1,13 +1,37 @@
 require "sinatra"
+require_relative "lib/leads"
+require_relative "lib/store"
+
+store = Store::InMemoryStore.new
 
 get "/" do
   erb :home
 end
 
 get "/lista-de-invitados" do
-  erb :guests
+  erb :guests, layout: false
 end
 
 get "/tests" do
   erb :tests
+end
+
+get "/registro" do
+  @form = Leads.register_form
+  erb :new_lead, layout: false
+end
+
+post '/registro' do
+  response = Leads.register_lead(params, store)
+  puts params.inspect
+  if response.success?
+    redirect to("/registro_exitoso")
+  else
+    @form = response.form
+    erb :new_lead, layout: false
+  end
+end
+
+get "/registro_exitoso" do
+  erb :registered_lead, layout: false
 end
