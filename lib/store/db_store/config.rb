d0cda0bb692ed837e1915822d355a525997d723a
bhserna <bhserna@gmail.com>
2017-01-22 18:29:49 -0600

    config

--- lib/store/db_store/config.rb
+++ lib/store/db_store/config.rb
@@ -1,3 +1,12 @@
 require 'active_record'
 
-ActiveRecord::Base.establish_connection(adapter: "postgresql", database: ENV['DATABASE_URL'])
+db = URI.parse(ENV['DATABASE_URL'] || 'postgres:///localhost/mydb')
+
+ActiveRecord::Base.establish_connection(
+   :adapter  => db.scheme == 'postgres' ? 'postgresql' : db.scheme,
+   :host     => db.host,
+   :username => db.user,
+   :password => db.password,
+   :database => db.path[1..-1],
+   :encoding => 'utf8'
+ )
