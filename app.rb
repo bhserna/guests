a491ae0f3c100dd0e857bf472404237e37bd38cf
bhserna <bhserna@gmail.com>
2017-02-01 09:32:50 -0600

    add analytics to articles

--- app.rb
+++ app.rb
@@ -9,20 +9,20 @@ set :partial_template_engine, :erb
 store = Store::Leads
 
 get "/" do
-  erb :home
+  erb :home, layout: :home_layout
 end
 
 get "/lista-de-invitados" do
-  erb :guests, layout: false
+  erb :guests
 end
 
 get "/tests" do
-  erb :tests
+  erb :tests, layout: false
 end
 
 get "/registro" do
   @form = Leads.register_form
-  erb :"lead_register/new", layout: false
+  erb :"lead_register/new"
 end
 
 post '/registro' do
@@ -32,17 +32,17 @@ post '/registro' do
     redirect to("/registro_exitoso")
   else
     @form = response.form
-    erb :"lead_register/new", layout: false
+    erb :"lead_register/new"
   end
 end
 
 get "/registro_exitoso" do
-  erb :"lead_register/registered", layout: false
+  erb :"lead_register/registered"
 end
 
 get "/registro_articulos" do
   @form = Leads.register_form
-  erb :"lead_register/new_from_article", layout: false
+  erb :"lead_register/new_from_article"
 end
 
 post '/registro_articulos' do
@@ -52,16 +52,16 @@ post '/registro_articulos' do
     redirect to("/registro_articulos_exitoso")
   else
     @form = response.form
-    erb :"lead_register/new_from_article", layout: false
+    erb :"lead_register/new_from_article"
   end
 end
 
 get "/registro_articulos_exitoso" do
-  erb :"lead_register/registered_from_article", layout: false
+  erb :"lead_register/registered_from_article"
 end
 
 get "/articles/preguntas-para-reducir-su-lista-de-invitados" do
   @page_title = "Preguntas para reducir su lista de invitados"
   @meta_description = "Una pequeña guía de preguntas para ayudarlos a reducir su lista de invitados"
-  erb :"articles/article-1"
+  erb :"articles/article-1", layout: :home_layout
 end
